257e5fcc79d3406ed01ae08fa7d846f5fdeb0468
Ganesh Salgar <ganesh.salgar.37.4@mountblue.tech>
2026-06-18 22:13:05 +0530

    Add custom myMap function and enhance shallow and deep copy examples

---
+++ CustomiseFunction/customMapFunction.js
@@ -0,0 +1,25 @@
+
+
+
+Array.prototype.myMap = function(callback){
+    const result = [];
+
+    for(let i = 0; i < this.length; i++){
+        result.push(callback(this[i], i, this));
+    }
+
+
+    return result;
+}
+
+let arr = [1,2,3,4];
+
+
+const newArr  = arr.myMap((num) => {
+    return num * 2;
+});
+
+
+
+
+console.log(newArr);
--- shallowAndDeepCopy/script.js
+++ shallowAndDeepCopy/script.js
@@ -3,3 +3,21 @@ const Username1 = 'Ganesh'
 const Username2 = Username1;
 
 console.log(Username1)
+
+
+console.log({} === {});
+console.log({} == {})
+
+
+console.log([] == []);
+console.log([] === []);
+
+
+const obj ={
+    name : "Ganesh"
+}
+
+
+
+
+const arr = [1 ,2,3];
